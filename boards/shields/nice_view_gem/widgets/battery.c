57ddc3d45af1caf7bdbf082ddef32b61fbbcc84a
Lovro Oreskovic <lovro@oreskovic.me>
2026-06-24 18:39:31 +0200

    feat: move bars to edges, add charging icon between them, remove SIG label
    - Battery bars now flush left (x=0) and right (x=68-bar_w)
    - Lightning bolt icon drawn between bars only when charging
    - Output status icon centered at top, SIG label removed

--- boards/shields/nice_view_gem/widgets/battery.c
+++ boards/shields/nice_view_gem/widgets/battery.c
@@ -7,34 +7,32 @@ LV_IMG_DECLARE(bolt);
 void draw_battery_status(lv_obj_t *canvas, const struct status_state *state) {
 #if !IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
     // Battery bar dimensions
-    const int bar_w = 24;
+    const int bar_w = 26;
     const int bar_h = 12;
-    const int bar_spacing = 4;
-    const int total_w = bar_w * 2 + bar_spacing;
-    const int start_x = (68 - total_w) / 2;
-    const int bar_y = 18;
+    const int bar_y = 16;
+    const int left_x = 0;
+    const int right_x = 68 - bar_w;
 
     lv_draw_rect_dsc_t rect_dsc;
 
     // Draw left (central) battery bar outline
     lv_draw_rect_dsc_init(&rect_dsc);
     rect_dsc.bg_color = LVGL_FOREGROUND;
-    canvas_draw_rect(canvas, start_x, bar_y, bar_w, bar_h, &rect_dsc);
+    canvas_draw_rect(canvas, left_x, bar_y, bar_w, bar_h, &rect_dsc);
     lv_draw_rect_dsc_init(&rect_dsc);
     rect_dsc.bg_color = LVGL_BACKGROUND;
-    canvas_draw_rect(canvas, start_x + 1, bar_y + 1, bar_w - 2, bar_h - 2, &rect_dsc);
+    canvas_draw_rect(canvas, left_x + 1, bar_y + 1, bar_w - 2, bar_h - 2, &rect_dsc);
 
     // Left fill (drains to the left)
     int fill_w = (state->battery * (bar_w - 2)) / 100;
     if (fill_w > 0) {
         lv_draw_rect_dsc_init(&rect_dsc);
         rect_dsc.bg_color = LVGL_FOREGROUND;
-        int fill_x = start_x + 1 + (bar_w - 2 - fill_w);
+        int fill_x = left_x + 1 + (bar_w - 2 - fill_w);
         canvas_draw_rect(canvas, fill_x, bar_y + 1, fill_w, bar_h - 2, &rect_dsc);
     }
 
     // Draw right (peripheral) battery bar outline
-    int right_x = start_x + bar_w + bar_spacing;
     lv_draw_rect_dsc_init(&rect_dsc);
     rect_dsc.bg_color = LVGL_FOREGROUND;
     canvas_draw_rect(canvas, right_x, bar_y, bar_w, bar_h, &rect_dsc);
@@ -53,29 +51,29 @@ void draw_battery_status(lv_obj_t *canvas, const struct status_state *state) {
         }
     }
 
-    // Labels
+    // Labels below bars
     lv_draw_label_dsc_t label_dsc;
     init_label_dsc(&label_dsc, LVGL_FOREGROUND, &pixel_operator_mono, LV_TEXT_ALIGN_CENTER);
-    canvas_draw_text(canvas, start_x, bar_y + bar_h + 2, bar_w, &label_dsc, "L");
+    canvas_draw_text(canvas, left_x, bar_y + bar_h + 2, bar_w, &label_dsc, "L");
     canvas_draw_text(canvas, right_x, bar_y + bar_h + 2, bar_w, &label_dsc, "R");
 
     // Percentage text below bars
     char text[10] = {};
     sprintf(text, "%i%%", state->battery);
-    canvas_draw_text(canvas, start_x - 2, bar_y + bar_h + 13, bar_w + 4, &label_dsc, text);
+    canvas_draw_text(canvas, left_x - 1, bar_y + bar_h + 13, bar_w + 2, &label_dsc, text);
 
     if (state->peripheral_connected) {
         sprintf(text, "%i%%", state->peripheral_battery);
-        canvas_draw_text(canvas, right_x - 2, bar_y + bar_h + 13, bar_w + 4, &label_dsc, text);
+        canvas_draw_text(canvas, right_x - 1, bar_y + bar_h + 13, bar_w + 2, &label_dsc, text);
     } else {
-        canvas_draw_text(canvas, right_x - 2, bar_y + bar_h + 13, bar_w + 4, &label_dsc, "--");
+        canvas_draw_text(canvas, right_x - 1, bar_y + bar_h + 13, bar_w + 2, &label_dsc, "--");
     }
 
-    // Charging indicator
+    // Lightning icon between bars (only when charging)
     if (state->charging) {
         lv_draw_image_dsc_t img_dsc;
         lv_draw_image_dsc_init(&img_dsc);
-        canvas_draw_img(canvas, start_x + bar_w + 2, bar_y - 8, &bolt, &img_dsc);
+        canvas_draw_img(canvas, 30, bar_y + 1, &bolt, &img_dsc);
     }
 #else
     // Peripheral: simple battery display
